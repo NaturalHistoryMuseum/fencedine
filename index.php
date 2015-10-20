8c15b84d2bee90c5618daa0dab3cee2f08344725
Simon Rycroft <s.rycroft@nhm.ac.uk>
2015-10-20 16:00:52 +0100

    Redirect and ensure it's valid HTML.

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
+<!DOCTYPE html>
 <html>
+<head>
+<title>Offline</title>
+<meta http-equiv="refresh" content="30">
+</head>
 <body>
 <h1>The Fencedine service has been taken offline. Please update your Scratchpads code.</h1>
 </body>
